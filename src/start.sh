f209003b89d075b298341297a0e4a3ba4308e547
Ubuntu <ubuntu@ip-172-31-32-197.us-west-2.compute.internal>
2017-12-01 05:39:17 +0000

    aquadocked

--- src/start.sh
+++ src/start.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# start MySQL
+# Start MySQL
 service mysql start
 mysql -e 'create database developer;'
 mysql developer < ~/aquarium_basic_db.dump
 rm -rf ~/aq_database_dump.sql
 
-# set up database
+# Set up database
 cd ~/aquarium
 export S3_BUCKET_NAME=""
 export AWS_ACCESS_KEY_ID=""
 export AWS_SECRET_ACCESS_KEY=""
 export AWS_REGION=""
-/bin/bash -l -c 'rake db:migrate'
+/bin/bash -l -c 'rake db:migrate' 
 
-# start servers
-/bin/bash -l -c 'rails s &'
+# Start servers
 /bin/bash -l -c "rails runner 'Krill::Server.new.run(3501)' &"
+/bin/bash -l -c 'rails s'
 
